5a15cf216ca9f177d3332a9036235a5be15c8e38
zhouyu <zhouyuzf@163.com>
2020-03-20 15:04:18 +0800

    test

---
+++ dsl_test/func_dsl_test.go
@@ -0,0 +1,39 @@
+package dsl_test
+import (
+	. "github.com/agiledragon/gomonkey"
+	. "github.com/agiledragon/gomonkey/dsl"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestPbBuilderFunc(t *testing.T) {
+	Convey("TestPbBuilderFunc", t, func() {
+
+		Convey("first dsl", func() {
+			patches := NewPatches()
+			defer patches.Reset()
+			patchBuilder := NewPatchBuilder(patches)
+
+			patchBuilder.
+				Func(Belong).
+				Stubs().
+				With(Eq("zxl"), Any()).
+				Will(Return(true)).
+				Then(Repeat(Return(false), 2)).
+				End()
+
+			flag := Belong("zxl", []string{})
+			So(flag, ShouldBeTrue)
+
+			defer func() {
+				if p := recover(); p != nil {
+					str, ok := p.(string)
+					So(ok, ShouldBeTrue)
+					So(str, ShouldEqual, "input paras ddd is not matched")
+				}
+			}()
+			Belong("ddd", []string{"abc"})
+		})
+
+	})
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module gomonkeytest
+
+go 1.12
+
+require (
+	github.com/agiledragon/gomonkey v2.0.1+incompatible
+	github.com/smartystreets/goconvey v1.6.4
+)
